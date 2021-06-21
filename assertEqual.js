30a564dcc87907a232dd3fd47885cd1f85711706
Afsanul Haque Khan <afsanhk@gmail.com>
2021-06-21 17:50:08 +0000

    Made an assertion file which checks if two inputs are the same data type and content. Learned how to use the emoji keyboard as a result too!

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+
+const assertEqual = function(actual, expected) {
+  return actual === expected ? console.log(`✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Afsan","Afsan");
+assertEqual(1,1);
+assertEqual(2,1);
+
