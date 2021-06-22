6dc4a4f27eb24263884dc0be787aaf9958dfd378
Afsanul Haque Khan <afsanhk@gmail.com>
2021-06-22 21:54:45 +0000

    Created findKeyByValue.js which takes in two arguments: an object and a reference value, then returns the object key which contains that value.

---
+++ findKeyByValue.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  return actual === expected ? console.log(`✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = function (inputObject, refValue) {
+  for (let key in inputObject) {
+    if (inputObject[key] === refValue) {
+      return key;
+    }
+  } 
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
