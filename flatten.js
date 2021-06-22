e6cebc3f23fb4ccfd32f6955ea9931afbfc7b639
Afsanul Haque Khan <afsanhk@gmail.com>
2021-06-22 17:20:41 +0000

    Created flatten.js to flatten one level of nested arrays into a single array. Stretch for W1D2 at LHL.

---
+++ flatten.js
@@ -0,0 +1,37 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(array1, array2) {
+  return eqArrays(array1,array2) ? console.log(`✅ Assertion Passed: Array 1 === Array 2`) : console.log(`❌ Assertion Failed: Array 1 !== Array 2`);
+};
+
+const flatten = function (inputArray) {
+  for (let i = 0; i < inputArray.length; i++) {
+    
+    if (Array.isArray(inputArray[i])) {
+      let temporaryStorage = inputArray[i];
+      inputArray.splice(i,1);
+      for (let j = 0; j < temporaryStorage.length; j++) {
+        inputArray.splice(i+j,0,temporaryStorage[j]);
+      }
+    }
+  }
+  return inputArray;
+};
+
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
+console.log(flatten([1,[3,6,7],[8,9,0],2,3,4,[3,4,7],7]));
+
+assertArraysEqual(flatten([1,2,3,[5,8,9]]),[1,2,3,5,8,9]);
