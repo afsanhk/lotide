a36ea0ede4c18fd57bb192309498c2c847ce29e1
Afsanul Haque Khan <afsanhk@gmail.com>
2021-06-22 21:33:00 +0000

    Created countLetters.js to count the number of times a letter is repeated (upper case and lower case counted separately) in a string. Does not count spaces and returns an object with key value pairs showing letters and count.

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  return actual === expected ? console.log(`✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const countLetters = function (str) {
+  const results = {};
+
+  for (let letter of str) {
+    
+    if(letter !== ' '){
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+
+  return results;
+};
+
+const letters = countLetters('Afsanul Haque Khan');
+console.log(letters);
+assertEqual(letters['a'],3);
