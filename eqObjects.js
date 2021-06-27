8c77eea6160c54bc04c4c1ed53d9fc80384261e0
Afsanul Haque Khan <afsanhk@gmail.com>
2021-06-27 22:01:53 +0000

    Updated eqObjects to parse through nested objects for equality as well using recursion!

--- eqObjects.js
+++ eqObjects.js
@@ -1,3 +1,5 @@
+const inspect = require('util').inspect;
+
 const assertEqual = function(actual, expected) {
   return actual === expected ? console.log(`✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
 };
@@ -24,6 +26,12 @@ const eqObjects = function(object1, object2) {
 
   if (object1Keys.length === object2Keys.length) {
     for (let key of object1Keys) {
+    // RECURISVE CASE -- If object[key] is another object
+      if (typeof(object1[key]) === 'object' && typeof(object2[key]) === 'object' && Array.isArray(object1[key]) === false && Array.isArray(object2[key]) === false) {
+        return eqObjects(object1[key],object2[key]);
+      }
+  
+      // BASE CASE -- If Object[key] is not another object (not incl. arrays)
       if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
         if (!(eqArrays(object1[key],object2[key]))) { // Why '!'? If this is true, follow the else if statement.
           return false;
@@ -40,19 +48,56 @@ const eqObjects = function(object1, object2) {
 };
 
 
-//  PRIMITIVES
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
+// //  PRIMITIVE TESTS
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
 
-assertEqual(eqObjects(ab, ba),true); // => true
+// assertEqual(eqObjects(ab, ba),true); // => true
 
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc),false); // => false
+// const abc = { a: "1", b: "2", c: "3" };
+// assertEqual(eqObjects(ab, abc),false); // => false
 
-// ARRAYS
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc),true); // => true
+// // ARRAY TESTS
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// assertEqual(eqObjects(cd, dc),true); // => true
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2),false); // => false
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// assertEqual(eqObjects(cd, cd2),false); // => false
+
+
+// STRETCH -- NESTED OBJECT TESTS
+console.log(assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }),true)); // => true
+console.log(assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }),false)); // => false
+console.log(assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }),false)); // => false
+
+
+
+// STRETCH -- MORE TESTS:
+
+const x = {
+  'number': 2,
+  'name': 'Tragedy',
+  'array': [1,3,5,7],
+  'obj': {a:{b:{c:{d:{e:{f:{condition: 'poor'}}}}}}}
+};
+
+const y = {
+  'obj': {a:{b:{c:{d:{e:{f:{condition: 'poor'}}}}}}} ,
+  'number': 2,
+  'name': 'Tragedy',
+  'array': [1,3,5,7]
+};
+
+const z = {
+  'obj': {f:{e:{d:{c:{b:{a:{condition: 'poor'}}}}}}} ,
+  'number': 2,
+  'name': 'Tragedy',
+  'array': [1,3,5,7]
+};
+
+console.log(assertEqual(eqObjects({ a: { x:0, y: 35, z: 1 }, b: 2 }, { a: { z: 1, x:0,y:35 }, b: 2 }),true)); // => true
+console.log(assertEqual(eqObjects({ a: { x:0, y: 'chicken nuggets', z: 1 }, b: 2 }, { a: { z: 1, x:0,y:35 }, b: 2 }),false)); // => false
+
+console.log(assertEqual(eqObjects(x,y),true)); // => true
+console.log(assertEqual(eqObjects(x,z),false)); // => false
