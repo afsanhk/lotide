2015e174e88ef06e52f0503ef2d1b819b1f88f73
Afsanul Haque Khan <afsanhk@gmail.com>
2021-06-28 13:31:23 +0000

    Refactored assertArraysEqual.js by using module.export and require instead of copying and pasting functions. Created a separate test file within the test subdirectory for the function.

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,11 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+// TEST CODE
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])); // => true
+console.log(assertArraysEqual([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3])); // => false
+
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])); // => should PASS
+console.log(assertArraysEqual([1, 2, 3], [3, 2, 1])); // False
