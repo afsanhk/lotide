e5ce32276d69d797c878d0a24d1ba60e37a41fac
Afsanul Haque Khan <afsanhk@gmail.com>
2021-06-28 19:17:18 +0000

    Refactored eqArraysTest.js and added eqObjectsTest.js

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,32 +1,62 @@
 const eqArrays = require('../eqArrays');
 const assertEqual = require('../assertEqual');
-
-// TESTS
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should fail
+const assert = require('chai').assert;
 
 
-// STRETCH - NESTED ARRAYS WITH RECURSION
+describe(`#eqArrays()`, () => {
 
-console.log(assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]),true)); // => true
-console.log(assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]),false)); // => false
-console.log(assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]),false)); // => false
+  describe(`Non-nested arrays`, () => {
 
-// STRETCH - EXTRA TESTS
+    it(`Should return true comparing [1,2,3] with [1,2,3]`, () => {
+      assert.isTrue(eqArrays([1, 2, 3], [1, 2, 3]));
+    });
 
-const a = [[[[[[1,2,3,],4],5],6],7],9, [10], [13], [[[[18]]]]];
-const b = [[[[[[1,2,3,],4],5],6],7],9, [10], [13], [[[[18]]]]];
-const c = [[[[[[1,2,3,],4],5],6],7], [10], [13], [[[[18]]]]];
-console.log(assertEqual(eqArrays(a,b),true));
-console.log(assertEqual(eqArrays(a,c),false));
+    it(`Should return false comparing [3,2,1] with [1,2,3]`, () => {
+      assert.isFalse(eqArrays([3,2,1], [1, 2, 3]));
+    });
 
-const d = ['hello', [[['I'], ['am']]], ['nested']];
-const e = ['hello', [[['I'], ['am']]], ['nested']];
-console.log(assertEqual(eqArrays(d,e),true));
+    it(`Should return false comparing [1,2,3] with [1,2,"3"]`, () => {
+      assert.isFalse(eqArrays([1, 2, 3], [1, 2, "3"]));
+    });
+
+  });
+
+  describe(`Nested arrays`, () => {
+    it(`Should return true comparing [[2, 3], [4]] with [[2, 3], [4]]`, () => {
+      assert.isTrue(eqArrays([[2, 3], [4]],[[2, 3], [4]]));
+    });
+
+    it(`Should return false comparing [[2, 3], [4]] with [[2, 3], [4, 5]]`, () => {
+      assert.isFalse(eqArrays([[2, 3], [4]],[[2, 3], [4, 5]]));
+    });
+    
+    it(`Should return false comparing [[2, 3], [4]] with [[2, 3], 4]`, () => {
+      assert.isFalse(eqArrays([[2, 3], [4]],[[2, 3], 4]));
+    });
+
+    it(`Should return false comparing [[[[[[1,2,3,],4],5],6],7],9, [10], [13], [[[[18]]]]] with [[[[[[1,2,3,],4],5],6],7], [10], [13], [[[[18]]]]]`, () => {
+      assert.isFalse(eqArrays([[[[[[1,2,3,],4],5],6],7],9, [10], [13], [[[[18]]]]],[[[[[[1,2,3,],4],5],6],7], [10], [13], [[[[18]]]]]));
+    });
+
+  });
+
+});
+
+// // STRETCH - NESTED ARRAYS WITH RECURSION
+
+// console.log(assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]),true)); // => true
+// console.log(assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]),false)); // => false
+// console.log(assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]),false)); // => false
+
+// // STRETCH - EXTRA TESTS
+
+// const a = [[[[[[1,2,3,],4],5],6],7],9, [10], [13], [[[[18]]]]];
+// const b = [[[[[[1,2,3,],4],5],6],7],9, [10], [13], [[[[18]]]]];
+// const c = [[[[[[1,2,3,],4],5],6],7], [10], [13], [[[[18]]]]];
+// console.log(assertEqual(eqArrays(a,b),true));
+// console.log(assertEqual(eqArrays(a,c),false));
+
+// const d = ['hello', [[['I'], ['am']]], ['nested']];
+// const e = ['hello', [[['I'], ['am']]], ['nested']];
+// console.log(assertEqual(eqArrays(d,e),true));
 
